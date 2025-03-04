ed7763a1b33b32567cb6feaf24c1982db3794c9e
C4ncino <193305@iberopuebla.mx>
2025-03-03 19:23:51 -0600

    feat: add i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "http://localhost:4321",
   integrations: [sitemap()],
-
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: true,
+    }
+  },
   vite: {
     plugins: [tailwindcss()],
   },
---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+  en: 'English',
+  esp: 'Español',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'nav.home': 'Inicio',
+    'nav.about': 'Sobre Mi',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About Me',
+  },
+} as const;
