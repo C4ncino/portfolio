cc7affef902767d4270414709f868168bcbab439
C4ncino <193305@iberopuebla.mx>
2025-03-18 12:27:48 -0600

    feat: add icons to project and upgrade 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ccancino.vercel.app/",
   output: "static",
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
