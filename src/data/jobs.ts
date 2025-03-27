e6f8d7c90df6b1522d14e896d45769148d8d88df
C4ncino <193305@iberopuebla.mx>
2025-03-27 17:04:30 -0600

    refractor: jobs info to new file

---
+++ src/data/jobs.ts
@@ -0,0 +1,81 @@
+export const jobsEs = [
+  {
+    position: "Interno de IT",
+    company: "Fisair",
+    startDate: "2023-05",
+    endDate: "2023-07",
+    description: [
+      "Desarrollé una aplicación móvil con <em>Ionic 5 y React 18</em> para la generación de reportes internos. El flujo de trabajo mejoró al poder gestionar reportes en cualquier momento reduciendo tiempos de respuesta.",
+      "Continue el proyecto como profesional independiente una vez finalizada mi estadía en la empresa."
+    ]
+  }, {
+    position: "Practicante de desarrollo",
+    company: "XUBE",
+    startDate: "2023-08",
+    endDate: "2023-12",
+    description: [
+      "Coordiné parte del diseño, selección de tecnologías y desarrollo de un sistema de control de recursos humanos.",
+      "Desarrollé el backend usando php con Laravel 10 y MySQL para la base de datos.",
+      "Apoyé en la capacitación de personal enseñando a mi equipo de trabajo el uso de tecnologías nuevas y mejores prácticas de programación."
+    ]
+  }, {
+    position: "Practicante de desarrollo",
+    company: "MEXabat",
+    startDate: "2024-01",
+    endDate: "2025-01",
+    description: [
+      "Participé en proyectos internos de RPA mediante el uso de diferentes plataformas como Automation Anywhere, Blue Prism y UiPath, así como automatización mediante el uso de Python y librerías como Selenium v4 y Pandas 2.2.",
+      "Colaboré en la creación de un framework interno de Python para agilizar el desarrollo en el área de AI y en la investigación de nuevas tecnologías.",
+      "Colaboré en proyectos de IoT, mejorando la documentación y la eficiencia del código. Además, brindé apoyo en un proyecto conjunto con el área de IA para desarrollar un sistema de visión por computadora."
+    ]
+  }, {
+    position: "Profesional independiente",
+    company: "",
+    startDate: "2023-08",
+    description: [
+      "Desarrollo proyectos para clientes personales dirigiendo un pequeño equipo.",
+      "He realizado aplicaciones híbridas usando React Native mediante Expo 52 y páginas web usando Astro 4 y Tailwind 3.",
+    ]
+  }
+]
+
+export const jobsEn = [
+  {
+    position: "IT Intern",
+    company: "Fisair",
+    startDate: "2023-05",
+    endDate: "2023-07",
+    description: [
+      "I developed a mobile application using Ionic 5 with React 18 for generating internal reports, improving the process by allowing report management at any time and reducing response times.",
+      "I continued the project as an independent professional after my time at the company ended.",
+    ]
+  }, {
+    position: "Practicante de desarrollo",
+    company: "XUBE",
+    startDate: "2023-08",
+    endDate: "2023-12",
+    description: [
+      "Coordinated part of the design, technology selection, and development of a human resource management system.",
+      "Developed the backend using PHP with Laravel 10 and MySQL for the database.",
+      "Assisted in staff training by teaching my team the use of new technologies and best programming practices.",
+    ]
+  }, {
+    position: "Practicante de desarrollo",
+    company: "MEXabat",
+    startDate: "2024-01",
+    endDate: "2025-01",
+    description: [
+      "Participated in internal RPA projects using various platforms such as Automation Anywhere, Blue Prism, and UiPath, as well as automation with Python and libraries like Selenium v4 and Pandas 2.2.",
+      "Contributed to the creation of an internal Python framework to streamline development in the AI department and research recent technologies.",
+      "Collaborated on IoT projects by improving documentation and code efficiency, Furthermore, I supported a joint project with the AI department to develop a computer vision system. ",
+    ]
+  }, {
+    position: "Independent Professional",
+    company: "",
+    startDate: "2023-08",
+    description: [
+      "Develop projects for personal clients, leading a small team.",
+      "Created hybrid applications using React Native with Expo 52 and websites with Astro 4 and Tailwind 3.",
+    ]
+  }
+]
